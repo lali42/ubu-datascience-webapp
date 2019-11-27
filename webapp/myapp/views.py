a7d67f4e47d90bad076aed6bb4294e14cebb60a5
sumet <sumet.sa.59@ubu.ac.th>
2019-11-27 15:42:19 +0700

    matmul

--- webapp/myapp/views.py
+++ webapp/myapp/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+import numpy as np
 
 # Create your views here.
+def convert_to_ndarray(x):
+    # y = np.random.random((6,5))
+    x = x.replace('[','')
+    x = x.replace(']','')
+    y=[]
+    for line in x.split('\n'):
+        y.append(list(map(float, line.split())))
+    return np.array(y)
+
+def matmul(req):
+    a = convert_to_ndarray('1 2 3\n6 7 8')
+    b = convert_to_ndarray('1 2 3\n6 7 8\n 1 5 6')
+    if req.method == 'POST':
+        print('POST')
+        print(req.POST['A'])
+        a = convert_to_ndarray(req.POST['A'])
+        print(req.POST['B'])
+        b = convert_to_ndarray(req.POST['B'])
+    else:
+        print('GET')
+    return render(req, 'myapp/matmul.html' , {'A':a,'B':b,'C':np.dot(a,b)})
+
